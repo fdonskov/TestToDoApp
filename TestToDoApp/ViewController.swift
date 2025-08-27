3fb627858cd883c1d025f6b5a9f4b2ba92a110c6
fdonsk0v <fedor@kolikosoft.com>
2025-08-27 19:45:29 +0300

    Files project.

---
+++ TestToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestToDoApp
+//
+//  Created by Fedor Donskov on 27.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
